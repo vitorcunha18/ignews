c1ba7f36d5641d5d2b61d18322ae097d4810bfc9
vitorcunha18 <cunhavitor18@gmail.com>
2022-07-11 15:56:35 -0500

    Estrutura do nextjs
    Pasta 01 - estrutura completo

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+
+      <Head>
+          <title> Home | Ig.News</title>
+      </Head>
+      <h1> 
+        holla world
+      </h1>
+    
+    </>
+  )
+}
